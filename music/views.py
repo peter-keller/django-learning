df81443abd19d748db951fd83d93f7615a304198
kellykeller1213 <keller.f.peter@gmail.com>
2017-12-10 22:49:48 +0100

    feat(views): lists all album titles from database

--- music/views.py
+++ music/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
+from .models import Album
 
 def index(request):
-    return HttpResponse("<h1>Music app</h1>")
+    all_albums = Album.objects.all()
+    html = ''
+    for album in all_albums:
+        url = '/music/' + str(album.id) + '/'
+        html += '<a href="' + url + '">' + album.album_title + '</a><br>'
+    return HttpResponse(html)
 
 
 def detail(request, album_id):
