8a599a9b37a2ccccf3432d8b048ac457f3128194
kellykeller1213 <keller.f.peter@gmail.com>
2017-12-14 23:30:29 +0100

    feat(views): get_object_or_404 instead of try catch

--- music/views.py
+++ music/views.py
@@ -1,16 +1,11 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Album
 
 def index(request):
     all_albums = Album.objects.all()
-    context = {'all_albums': all_albums}
-    return render(request, 'music/index.html', context)
+    return render(request, 'music/index.html', {'all_albums': all_albums})
 
 
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(id=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album not found")
+    album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
