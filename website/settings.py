085c452de886ff6031803ebd679941cf1b3d4a25
peter-keller <keller.f.peter@gmail.com>
2018-01-27 20:55:20 +0100

    feat(stuff)

--- website/settings.py
+++ website/settings.py
@@ -77,6 +77,7 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # db_from_env = dj_database_url.config(conn_max_age=500)
 # DATABASES['default'].update(db_from_env)
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
