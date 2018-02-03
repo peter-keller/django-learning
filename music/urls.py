f1d90603cb79f1a9b9196a50314252848e34af56
peter-keller <keller.f.peter@gmail.com>
2018-02-03 22:50:58 +0100

    feat(urls): registration endpoint

--- music/urls.py
+++ music/urls.py
@@ -8,17 +8,20 @@ app_name = 'music'
 urlpatterns = [
     # /music/
     path('', views.IndexView.as_view(), name='index'),
+
+
+    path('register', views.UserFormView.as_view(), name='register'),
     
-    # /music/712
+    # /music/712/
     path('<pk>/', views.DetailView.as_view(), name='detail'),
 
-    #/music/album/add/
+    # /music/album/add/
     path('album/add/', views.AlbumCreate.as_view(), name='album-add'),
 
-
+    # /music/album/2/
     path('album/<pk>/', views.AlbumUpdate.as_view(), name='album-update'),
 
-
+    # /music/album/2/delete/
     path('album/<pk>/delete/', views.AlbumDelete.as_view(), name='album-delete'),
 
     
