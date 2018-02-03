9d82ca8a66904cdcaeb857b9a1fc15e1b6575fad
peter-keller <keller.f.peter@gmail.com>
2018-02-03 22:18:35 +0100

    feat(views): UserFormView class created

--- music/views.py
+++ music/views.py
@@ -1,7 +1,11 @@
-from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from django.urls import reverse_lazy
+from django.views import generic
+from django.views.generic import View
 from .models import Album
+from .forms import UserForm
 
 
 class IndexView(generic.ListView):
@@ -31,3 +35,32 @@ class AlbumUpdate(UpdateView):
 class AlbumDelete(DeleteView):
     model = Album
     success_url = reverse_lazy('music:index')
+
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'music/registration_form.html'
+
+
+    # display blank form for new users without account
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+    
+
+    # process form data
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+
+            user = form.save(commit=False)
+
+            # cleaned data
+            username = form.cleaned_data['username']
+            username = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+
+
