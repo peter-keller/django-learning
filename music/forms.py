79a5c92fe6a2373951ea765e03e4b7ec109eb3da
peter-keller <keller.f.peter@gmail.com>
2018-03-18 22:35:52 +0000

    feat(changes) -

--- music/forms.py
+++ music/forms.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.models import User
-from .models import Album, Song
 from django import forms
 
 
@@ -9,17 +8,3 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
-
-
-class AlbumForm(forms.ModelForm):
-
-    class Meta:
-        model = Album
-        fields = ['artist', 'album_title', 'genre', 'album_logo']
-
-
-class SongForm(forms.ModelForm):
-
-    class Meta:
-        model = Song
-        fields = ['song_title', 'audio_file']
